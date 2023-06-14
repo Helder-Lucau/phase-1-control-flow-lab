9ca46b82b48d5e72a46159415a1a34f23be9ece6
Helder <helderlucau@gmail.com>
2023-06-14 20:28:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue <= 400) {
+    return "This one is on me!";
+  }
+  else if (someValue > 2000 && someValue < 2500) {
+    return "I will gladly take your thirty bucks.";
+  }else{
+    return "No can do.";
+  }
+}
+console.log(scuberGreetingForFeet(1500));
+function ternaryCheckCity(city){
+  // Write your code here!
+   return city == "NYC" ? "Ok, sounds good.": "No go.";
+}
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+      break;
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
